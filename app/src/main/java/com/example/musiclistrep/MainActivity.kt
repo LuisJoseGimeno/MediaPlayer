479900b6b80374c3103375afe0f24aff02d3bec3
LuisJoseGimeno <luisjosegimeno@gmail.com>
2024-04-27 13:19:26 +0200

    update to video

--- app/src/main/java/com/example/musiclistrep/MainActivity.kt
+++ app/src/main/java/com/example/musiclistrep/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.musiclistrep
 
 import MusicAdapter
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.musiclistrep.databinding.ActivityMainBinding
+import com.example.musiclistrep.databinding.SelectModeBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -11,6 +14,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.addButton.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            val dialogBinding = SelectModeBinding.inflate(layoutInflater)
+            builder.setView(dialogBinding.root)
+            val dialog = builder.create()
+            dialog.show()
+            dialogBinding.btnCancel.setOnClickListener {
+                dialog.dismiss()
+            }
+            dialogBinding.btnAudio.setOnClickListener {
+                // Aquí puedes agregar la lógica para seleccionar audio
+                // Por ejemplo, puedes abrir una actividad para grabar audio
+                // Luego, cierra el diálogo
+                dialog.dismiss()
+            }
+            dialogBinding.btnVideo.setOnClickListener {
+                // Aquí puedes agregar la lógica para seleccionar video
+                // Por ejemplo, puedes abrir una actividad para grabar video
+                // Luego, cierra el diálogo
+                dialog.dismiss()
+            }
+        }
 
         val rawResources = mutableListOf<Int>()
         val fields = R.raw::class.java.fields
---
+++ app/src/main/java/com/example/musiclistrep/SelectModeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.musiclistrep
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SelectModeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
